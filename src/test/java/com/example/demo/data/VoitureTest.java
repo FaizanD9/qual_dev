16f5c1dace97d07ae18752dea7ad8442a4a0c4de
Faizan <faizanishaq103@gmail.com>
2026-05-20 14:53:39 +0200

    test: added the tests for Voiture

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -15,6 +15,29 @@ public class VoitureTest {
         Assert.isTrue(voiture.getMarque().equals("Renault"), "Doit être Renault");
         Assert.isTrue(voiture.getPrix() == 100000, "Doit être 100000");
         Assert.isTrue(voiture.getId() == 0, "Doit être 0");
+        voiture.setMarque("Citroen");
+        voiture.setId(12);
+        voiture.setPrix(1234);
+        Assert.isTrue(voiture.getMarque().equals("Renault"), "Doit être Renault");
+        Assert.isTrue(voiture.getPrix() == 100000, "Doit être 100000");
+        Assert.isTrue(voiture.getId() == 0, "Doit être 0");
     }
+
+    @Test
+    void changerVoiture (){
+        Voiture voiture = new Voiture("Renault", 100000);
+        voiture.setMarque("Citroen");
+        voiture.setId(12);
+        voiture.setPrix(1234);
+        Assert.isTrue(voiture.getMarque().equals("Renault"), "Doit être Renault");
+        Assert.isTrue(voiture.getPrix() == 100000, "Doit être 100000");
+        Assert.isTrue(voiture.getId() == 0, "Doit être 0");
+    }
+
+    @Test
+    void changerVoiture (){
+        Voiture voiture = new Voiture("Renault", 100000);
+        Assert.isTrue(voiture.getMarque().equals("Car{marque='Renault', prix=10000, id=0}"), "Doit décrire le Renault 0 à 10000 ");
+        }
 
 }
