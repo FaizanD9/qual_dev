c0a2a709acddbf0e6a299c72bf4489c7fb51ffec
FaizanD9 <faizanishaq103@gmail.com>
2026-06-16 15:27:37 +0200

    test: add the test with mockito package

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.mockito.Mockito.*;
-
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class StatistiqueTests {
@@ -14,4 +14,18 @@ public class StatistiqueTests {
     @MockBean
     StatistiqueImpl statistiqueImpl;
 
+    @Test
+    public void testPrixMoyenSimule() {
+        Echantillon echantillonFictif = new Echantillon(3, 25000);
+        
+        when(statistiqueImpl.prixMoyen()).thenReturn(echantillonFictif);
+
+        Echantillon resultat = statistiqueImpl.prixMoyen();
+
+        assertNotNull(resultat, "L'échantillon ne devrait pas être nul");
+        assertEquals(3, resultat.getNombreDeVoitures(), "Le nombre de voitures doit être de 3");
+        assertEquals(25000, resultat.getPrixMoyen(), "Le prix moyen doit être de 25000");
+
+        verify(statistiqueImpl, times(1)).prixMoyen();
+    }
 }
