dd3fb37449612f80d62d9f33d0ecc863e0526864
FaizanD9 <faizanishaq103@gmail.com>
2026-06-16 15:33:13 +0200

    test: added all the tests with mock mvc

--- src/test/java/com/example/demo/web/WebTests.java
+++ src/test/java/com/example/demo/web/WebTests.java
@@ -13,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -27,4 +28,29 @@ class WebTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Test
+    public void testGetStatistiques() throws Exception {
+        Echantillon echantillonSimule = new Echantillon(2, 15000);
+        when(statistiqueImpl.prixMoyen()).thenReturn(echantillonSimule);
+
+        mockMvc.perform(get("/statistique"))
+                .andExpect(status().isOk()) 
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON)) 
+                .andExpect(jsonPath("$.nombreDeVoitures").value(2)) 
+                .andExpect(jsonPath("$.prixMoyen").value(15000))
+                .andDo(print()); 
+    }
+
+    @Test
+    public void testCreerVoiture() throws Exception {
+        String voitureJson = "{\"marque\":\"Ferrari\",\"prix\":100000}";
+
+        mockMvc.perform(post("/voiture")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(voitureJson))
+                .andExpect(status().isOk()) 
+                .andDo(print());
+
+        verify(statistiqueImpl, times(1)).ajouter(any(Voiture.class));
+    }
 }
